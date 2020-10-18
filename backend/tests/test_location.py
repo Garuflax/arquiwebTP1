838e8dbaab568e2d9c190c397a8bdbd11fd42360
linari facundo <facundo.linari@hotmail.com>
2020-10-17 23:17:36 -0300

    Agregar location/update

--- backend/tests/test_location.py
+++ backend/tests/test_location.py
@@ -3,10 +3,29 @@ from yeabackend.db import get_db
 
 @pytest.mark.parametrize('path', (
     '/location/create',
+    '/location/update',
     ))
 def test_login_required(client, path):
     response = client.post(path)
     assert response.status_code == 401
+
+def test_author_required(app, client, auth):
+    # change the post author to another user
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE location SET author_id = 2 WHERE id = 1')
+        db.commit()
+
+    auth.login()
+    # current user can't modify other user's post
+    assert client.post('/location/update', json={'id': 1, 'name': 'updated', 'maximum_capacity': 5}).status_code == 403
+
+@pytest.mark.parametrize('path', (
+    '/location/update',
+))
+def test_exists_required(client, auth, path):
+    auth.login()
+    assert client.post(path, json={'id': 2, 'name': 'updated', 'maximum_capacity': 5}).status_code == 404
 
 def test_create(client, auth, app):
     auth.login()
@@ -20,11 +39,23 @@ def test_create(client, auth, app):
         count = db.execute('SELECT COUNT(id) FROM location').fetchone()[0]
         assert count == 2
 
+def test_update(client, auth, app):
+    auth.login()
+    client.post('/location/update', json={'id': 1, 'name': 'updated', 'maximum_capacity': 5})
+
+    with app.app_context():
+        db = get_db()
+        location = db.execute('SELECT * FROM location WHERE id = 1').fetchone()
+        assert location['name'] == 'updated'
+        assert location['maximum_capacity'] == 5
+
 @pytest.mark.parametrize(('path', 'name', 'maximum_capacity', 'error'), (
     ('/location/create', '', 10, b'Name is required.'),
     ('/location/create', 'created', 0, b'Maximum capacity is required.'),
+    ('/location/update', '', 10, b'Name is required.'),
+    ('/location/update', 'created', 0, b'Maximum capacity is required.'),
 ))
 def test_create_update_validate(client, auth, path, name, maximum_capacity, error):
     auth.login()
-    response = client.post(path, json={'name': name, 'maximum_capacity': maximum_capacity})
+    response = client.post(path, json={'id': 1, 'name': name, 'maximum_capacity': maximum_capacity})
     assert error in response.data
--- backend/yeabackend/location.py
+++ backend/yeabackend/location.py
@@ -32,3 +32,47 @@ def create():
     db.commit()
     return jsonify(created=True,
                    message='Location created succesfully'), 201
+
+@bp.route('/update', methods=['POST'])
+def update():
+
+    login_required()
+
+    json_data = request.get_json()
+
+    #TODO better sanitation
+    #if 'name' not in json_data or 'maximum_capacity' not in json_data or 'id' not in json_data:
+    #       return abort(400, 'Missing field.')
+
+    name = json_data['name']
+    maximum_capacity = json_data['maximum_capacity']
+    id = json_data['id']
+
+    location = get_db().execute(
+        'SELECT p.id, name, maximum_capacity, author_id'
+        ' FROM location p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if location is None:
+        abort(404, "Location id {0} doesn't exist.".format(id))
+
+    check_author=True
+    if check_author and location['author_id'] != g.user['id']:
+        abort(403)
+
+    if not name:
+        abort(400, 'Name is required.')
+
+    if not maximum_capacity:
+        abort(400, 'Maximum capacity is required.')
+
+    db = get_db()
+    db.execute(
+        'UPDATE location SET name = ?, maximum_capacity = ?'
+        ' WHERE id = ?',
+        (name, maximum_capacity, id)
+    )
+    db.commit()
+    return jsonify(message='Location updated succesfully')
