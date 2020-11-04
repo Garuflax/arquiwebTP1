42419954499458b2abe8a1c919636a1f4a009e43
francastagna <fcastagna@dc.uba.ar>
2020-11-04 11:45:59 -0300

    agregue cantidad de locales y de usuarios al dashboard

--- frontend/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -11,6 +11,8 @@ export class AdminDashboardComponent implements OnInit {
 
   locations_data;
   users_data;
+  number_of_locations;
+  number_of_users;
 
   constructor(private adminService: AdminService) { }
 
@@ -20,12 +22,19 @@ export class AdminDashboardComponent implements OnInit {
 
     this.adminService.get_locations_data()
       .subscribe( 
-        (data) => {this.locations_data = data;}
+        (data) => {
+          this.locations_data = data;
+          this.number_of_locations = Object.keys(data).length;
+        }
     )
 
     this.adminService.get_users_data()
       .subscribe( 
-        (data) => {this.users_data = data;}
+        (data) => {
+          this.users_data = data;
+          this.number_of_users = Object.keys(data).length;
+          ;
+        }
     )
 
   }
