b3bd47ab78ee58b4d79277ebf7cd1029898a9cdd
francastagna <homero_franco@hotmail.com>
2020-11-20 15:26:34 -0300

    agregué css de location-detail

--- frontend/src/app/location/location.module.ts
+++ frontend/src/app/location/location.module.ts
@@ -6,7 +6,13 @@ import { LocationRoutingModule } from './location-routing.module';
 import { LocationsComponent } from './location-all/location-all.component';
 import { LocationDetailComponent } from './location-detail/location-detail.component';
 
-
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { MatButtonModule } from '@angular/material/button' 
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [LocationsComponent, LocationDetailComponent],
@@ -15,7 +21,14 @@ import { LocationDetailComponent } from './location-detail/location-detail.compo
     LocationRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: ''
-    })
+    }),
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [LocationsComponent, LocationDetailComponent]
 })
